58b4b3a39e58e167b4c89923f4deb758684e52b3
Ashish Kumar <63470984+AshishKumar-9318@users.noreply.github.com>
2020-11-01 00:07:38 +0530

    Add files via upload

---
+++ Program/first_game_vegetables.py
@@ -0,0 +1,144 @@
+import tkinter
+from tkinter import*
+import random
+from tkinter import messagebox
+from random import shuffle
+from PIL import Image, ImageTk
+import json
+score=0
+
+words=[]
+answer=[]
+data = json.load(open('word_collection.json', encoding='utf-8'))
+def getlist(category):
+    collection=data[category]
+    for word in collection:
+        answer.append(word)
+        shuffleword=list(word)
+        shuffle(shuffleword)
+        words.append(shuffleword)
+
+print(answer)
+getlist('vegetables')
+
+# print(words)
+
+num=random.randint(0,len(words)-1)
+
+def open_home():
+    import os
+    os.system('python main_frame.py')
+    quit()
+def closegame(e):
+    quit()
+
+def initial():
+    global words,answer,num,score
+
+    lb1.configure(text=words[num])
+    lb2.configure(text='Score='+str(score))
+
+
+def ans_check():
+    global words,num,answer,score
+    user_input=e1.get()
+    if user_input.lower()==answer[num].lower():
+        messagebox.showinfo("Success","yup this is right")
+        score=score+1
+        lb2.configure(text='Score='+str(score))
+        Reset()
+    else:
+        messagebox.showinfo("Error","nope this is not right")
+        e1.delete(0,END)
+        Reset()
+
+def Reset():
+    global words,answer,num
+    num=random.randint(0,len(words)-1)
+    lb1.configure(text=words[num])
+    e1.delete(0,END)
+
+root=tkinter.Tk()
+# w_width, w_height = 1300, 750 
+# s_width, s_height = root.winfo_screenwidth(), root.winfo_screenheight()
+# x, y = (s_width/2)-(w_width/2), (s_height/2)-(w_height/2)
+# root.geometry('%dx%d+%d+%d' % (w_width,w_height,x,y-30))
+# # root.atributes("-Fullscreen",True)
+root.attributes("-fullscreen",True)
+# # root.pack(bg='pink')
+wallpaper_label=Label(root)
+wallpaper_label.pack()
+
+ph_wallpaper = Image.open('../image/first_game_wallpaper.png')
+ph_wallpaper = ph_wallpaper.resize((1300, 720), Image.ANTIALIAS)
+ph_wallpaper = ImageTk.PhotoImage(ph_wallpaper)
+wallpaper_label.configure(image=ph_wallpaper)
+
+word_frame=Frame(root)
+word_frame.pack()
+word_frame.place(x=0,y=100,height=200,width=1280)
+
+
+lb1= Label(word_frame,font='Helvetica 100')
+lb1.pack(pady=10,ipady=20,ipadx=10)
+# lb1.place(x=200,y=200)
+
+lb2=Label(root,font='Helvetica 40')
+lb2.pack(pady=5,ipadx=10,ipady=10)
+lb2.place(x=520,y=40)
+
+lb3=Label(root)
+lb3.pack()
+lb3.place(x=570,y=544)
+
+# lb2=Label(root,font='aries 10')
+# lb2.pack(pady=5,ipady=2,ipadx=2)
+
+final_answer=StringVar()
+e1=Entry(root,justify='center',font=('Arial',20))
+e1.pack(ipady=5,ipadx=5)
+e1.place(x=420,y=320,width=400,height=50)
+
+ph_check = Image.open('../image/check.png')
+ph_check = ph_check.resize((100, 100), Image.ANTIALIAS)
+ph_check = ImageTk.PhotoImage(ph_check)
+
+button1=Button(root,text="        Check        ",command=ans_check,font=('Arial',20),image=ph_check)
+button1.pack(pady=40)
+button1.place(x=585,y=400)
+
+ph_reset = Image.open('../image/reset.png')
+ph_reset = ph_reset.resize((100, 100), Image.ANTIALIAS)
+ph_reset = ImageTk.PhotoImage(ph_reset)
+
+
+button2=Button(root,text="        Reset        ",command=Reset,font=('Arial',20),image=ph_reset)
+button2.pack()
+button2.place(x=450,y=565)
+
+ph_quit = Image.open('../image/quit.png')
+ph_quit = ph_quit.resize((100, 100), Image.ANTIALIAS)
+ph_quit = ImageTk.PhotoImage(ph_quit)
+
+
+Button(root,text="          Quit          ",command=lambda:quit(),font=('Arial',20),image=ph_quit).place(x=725,y=565)
+
+
+ph_symbol = Image.open('../image/symbol.png')
+ph_symbol = ph_symbol.resize((140, 140), Image.ANTIALIAS)
+ph_symbol = ImageTk.PhotoImage(ph_symbol)
+
+lb3.configure(image=ph_symbol,bd=0)
+initial()
+ph_quit1 = Image.open('../image/home_button.png')
+ph_quit1 = ph_quit1.resize((50, 50), Image.ANTIALIAS)
+ph_quit1 = ImageTk.PhotoImage(ph_quit1)
+Button(root,text="Home",command=open_home,font=('Arial',10),image=ph_quit1,bd=0,bg='black',activebackground='black').place(x=1220,y=00)
+
+# ph_quit2 = Image.open('quit_button.png')
+# ph_quit2 = ph_quit2.resize((50, 50), Image.ANTIALIAS)
+# ph_quit2 = ImageTk.PhotoImage(ph_quit2)
+# Button(root,text='Quit',font=('Arial',10),command=lambda:quit(),image=ph_quit2,bd=0,bg='black',activebackground='black').place(x=0,y=0)
+
+root.bind("<Escape>",closegame)
+root.mainloop()
---
+++ Program/fourth_game_level5.py
@@ -0,0 +1,161 @@
+import tkinter
+from tkinter import*
+import random
+from tkinter import messagebox
+from random import shuffle
+from PIL import Image, ImageTk
+
+root=Tk()
+root.geometry("1200x1000")
+root.attributes("-fullscreen",True)
+
+def closegame(e):
+	quit()
+
+def clear():
+	e1.delete(0,END)
+	e2.delete(0,END)
+	e3.delete(0,END)
+
+
+def next_level_with_wrong():
+	messagebox.showinfo("Next Level",'Next Level is Locked Firstly Solved this')
+
+def next_level_with_right():
+	import os
+	os.system('python main_frame.py')
+	quit()
+
+def ans_check():
+	user_answer1=e1.get()
+	main_answer1='alarmclock'
+	user_answer2=e2.get()
+	main_answer2='painting'
+	user_answer3=e3.get()
+	main_answer3='books'
+	if(user_answer2==main_answer2):
+		if(user_answer1==main_answer1):
+			if(user_answer3==main_answer3):
+				messagebox.showinfo("Well Done",'You have a Right Answer')
+				Button(root,text="      Next      ",font=('Arial',20),command=next_level_with_right,image=ph_next).place(x=1080,y=450)
+				clear()
+				hint()
+			else:
+				messagebox.showinfo("Nope",'Better Luck Next Time')
+				clear()
+	else:
+		messagebox.showinfo("Nope",'Better Luck Next Time')
+		clear()
+
+def hint():
+	import os
+	os.system('python hint_crossword_level5.py')
+
+wallpaper_label=Label(root)
+wallpaper_label.pack()
+
+ph_wallpaper = Image.open('../image/first_game_wallpaper.png')
+ph_wallpaper = ph_wallpaper.resize((1300, 720), Image.ANTIALIAS)
+ph_wallpaper = ImageTk.PhotoImage(ph_wallpaper)
+wallpaper_label.configure(image=ph_wallpaper)
+
+frame1 = Frame(root, bg='White')
+# frame2 = Frame(root, bg='green')
+frame1.place(x=180,y=20)
+# frame2.pack(fill=X, side=BOTTOM)
+
+lb1=Label(frame1)
+lb1.pack()
+
+
+# OperationFrame = Frame(frame2, bg='blue',width=425,height=400)
+# OperationFrame.grid(row=0,column=0)
+
+# OperationFrame1 = Frame(frame2, bg='pink',width=425,height=400)
+# OperationFrame1.grid(row=0,column=1)
+
+# OperationFrame2 = Frame(frame2, bg='black',width=430,height=400)
+# OperationFrame2.grid(row=0,column=2)
+
+
+
+# leftframe = Frame(frame2, bg='blue', width=500, height=400)
+# leftframe.pack()
+
+e1=Entry(root,justify='center',font=('Arial',20))
+e1.pack()
+e1.place(height=40,width=300,x=480,y=350)
+label_e1=Label(root,text=" A L _ _ M C _ _ C _ ")
+label_e1.pack()
+label_e1.place(x=480,y=330)
+
+e2=Entry(root,justify='center',font=('Arial',20))
+e2.pack()
+e2.place(height=40,width=300,x=480,y=420)
+label_e2=Label(root,text="_ _ I _ T _ _ G")
+label_e2.pack()
+label_e2.place(x=480,y=400)
+
+e3=Entry(root,justify='center',font=('Arial',20))
+e3.pack()
+e3.place(height=40,width=300,x=480,y=490)
+label_e3=Label(root,text="_ O _ _ S")
+label_e3.pack()
+label_e3.place(x=480,y=470)
+
+
+ph_submit = Image.open('../image/submit.png')
+ph_submit = ph_submit.resize((80, 80), Image.ANTIALIAS)
+ph_submit = ImageTk.PhotoImage(ph_submit)
+
+lb3=Label(root)
+lb3.pack()
+lb3.place(x=570,y=544)
+
+Button(root,text="    Submit    ",font=('Arial', 20),command=ans_check,image=ph_submit).place(x=250,y=450)
+
+ph_next = Image.open('../image/next.png')
+ph_next = ph_next.resize((80, 80), Image.ANTIALIAS)
+ph_next = ImageTk.PhotoImage(ph_next)
+
+Button(root,text="      Next      ",font=('Arial',20),command=next_level_with_wrong,image=ph_next).place(x=1080,y=450)
+
+ph_hint = Image.open('../image/hint.png')
+ph_hint = ph_hint.resize((80, 80), Image.ANTIALIAS)
+ph_hint = ImageTk.PhotoImage(ph_hint)
+
+Button(root, text='     Hint     ', font=('Arial', 20),command=hint,image=ph_hint).place(x=940, y=450)
+
+ph_quit = Image.open('../image/quit.png')
+ph_quit = ph_quit.resize((80, 80), Image.ANTIALIAS)
+ph_quit = ImageTk.PhotoImage(ph_quit)
+
+Button(root, text='     Quit     ', font=('Arial', 20),command=lambda:quit(),image=ph_quit).place(x=110, y=450)
+
+lb2=Label(frame1,text="   Level 1  ",font=('Arial 20'))
+lb2.pack()
+lb2.place(x=550,y=365)
+
+
+
+ph = Image.open('../image/crossword5.jpeg')
+ph = ph.resize((900, 300), Image.ANTIALIAS)
+ph = ImageTk.PhotoImage(ph)
+lb1.configure(image=ph)
+
+ph_symbol = Image.open('../image/symbol.png')
+ph_symbol = ph_symbol.resize((140, 140), Image.ANTIALIAS)
+ph_symbol = ImageTk.PhotoImage(ph_symbol)
+
+lb3.configure(image=ph_symbol,bd=0)
+def open_home():
+	import os
+	os.system('python main_frame.py')
+	quit()
+
+ph_quit1 = Image.open('../image/home_button.png')
+ph_quit1 = ph_quit1.resize((50, 50), Image.ANTIALIAS)
+ph_quit1 = ImageTk.PhotoImage(ph_quit1)
+Button(root,text="Home",command=open_home,font=('Arial',10),image=ph_quit1,bd=0,bg='black',activebackground='black').place(x=1220,y=00)
+root.bind("<Escape>", closegame)
+root.mainloop()
